3cf730c4c101dda5eb904dfb4374c73f20d254ba
Richard Phong <richard.phong424@gmail.com>
2022-02-18 16:47:02 -0600

    added email

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -10,6 +10,7 @@ const Contact = () => (
     <Main style={{margin:'0 0 3rem 0'}}>
         <div id="contact">
             <h1 class="bold">Contact</h1>
+            <h3>You can email me at richard.phong424@gmail.com!</h3>
             <a href="https://github.com/rphong" target="_blank" rel="noreferrer"><AiFillGithub className='icon' size={'1x'}/></a>
             <a href="https://www.linkedin.com/in/richard-phong-36b297220/" target="_blank" rel="noreferrer"><AiFillLinkedin className='icon' size={'1x'}/></a>
             <a href="https://github.com/rphong" target="_blank" rel="noreferrer"><RiNewspaperLine className='icon' size={'1x'}/></a>

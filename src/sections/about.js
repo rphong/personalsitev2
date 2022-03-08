28b2881c294f73e701333a8dcc7837489247ba69
Richard Phong <richard.phong424@gmail.com>
2022-03-07 22:35:15 -0600

    increasing font weight, fixing typos/alignment stuff

--- src/sections/about.js
+++ src/sections/about.js
@@ -6,20 +6,20 @@ import Wave from '../components/Wave';
 
 const About = () => (
 
-    <Main style={{margin:'0 0 0 0'}} id="about">
+    <Main style={{ margin: '0 0 0 0' }} id="about">
         <div id="about" >
             <div id="imgMeContainer">
                 <img src={Me} alt="me" id="meImg"></img>
-            <div id="imgMeBg"></div>
+                <div id="imgMeBg"></div>
             </div>
             <h1 className="bold">Hi I'm <span>Richard!</span></h1>
             <h2>
-                I'm currently a student at the University of Houston, majoring in CS. I enjoy 
-                exploring cs related fields such as game development and competitive programming. Currently, I'm learning
+                I'm currently a student at the University of Houston, majoring in CS. I enjoy
+                exploring CS related fields such as game development and competitive programming. Currently, I'm learning
                 web development along with a bit of 3d modeling!
             </h2>
         </div>
-        <Wave/>
+        <Wave />
     </Main>
 );
 
